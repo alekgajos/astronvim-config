7c57fa435643403f20f6d11cef47541b1fa14416
Aleksander Gajos <agajos@oanda.com>
2024-08-30 10:55:41 +0200

    Remove hardcoded bash path + add linters

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -31,7 +31,7 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
-        shell = "/opt/homebrew/bin/bash -l",
+        shell = "bash -l",
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
